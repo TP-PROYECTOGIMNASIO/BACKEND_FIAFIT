4a878c3daaa02bfad6cd803950a68ce5826c9de2
PaoloDiaz99 <pda99@hotmail.com>
2024-08-28 14:22:52 -0500

    feature/hu-tp-17_v2.0

---
+++ lambda-gestionar-productos/HU-TP-17/index.js
@@ -0,0 +1,53 @@
+import pkg from 'pg';
+
+// Configuración de la conexión
+const pool = new pkg.Pool({
+  user: 'db_gym_render_user',
+  host: 'dpg-cr5568ij1k6c73934m10-a.oregon-postgres.render.com',
+  database: 'db_gym_render',
+  password: 'LusVdIcmARRFj7nY76BbOj9MzQ2Y33I5',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false // Solo en desarrollo. En producción, configura esto correctamente.
+  }
+});
+
+
+export const handler = async (event) => {
+  try {
+    const productTypeId = event.queryStringParameters?.product_type_id;
+
+    let query;
+    let values = [];
+
+    if (productTypeId) {
+      // Si se proporciona un product_type_id, filtrar por ese producto
+      query = 'SELECT * FROM t_product_types WHERE product_type_id = $1';
+      values = [productTypeId];
+    } else {
+      // Si no se proporciona un product_type_id, devolver todos los productos
+      query = 'SELECT * FROM t_product_types ORDER BY product_name ASC';
+    }
+
+    const result = await pool.query(query, values);
+
+    if (result.rows.length === 0) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ message: 'Tipo de producto no encontrado' }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.rows),
+    };
+  } catch (err) {
+    console.error('Error en la operación', err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
